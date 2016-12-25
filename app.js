519e96d919bdf22d1e6a7099c08b49d6e4634c35
jfmolano1587 <jose.francisco.molano@gmail.com>
2016-12-25 18:37:11 -0500

    Angular ng-click

--- app.js
+++ app.js
@@ -10,5 +10,8 @@ function($scope){
   {title: 'post 3', upvotes: 15},
   {title: 'post 4', upvotes: 9},
   {title: 'post 5', upvotes: 4}
-];
+  ];
+  $scope.addPost = function(){
+  $scope.posts.push({title: 'A new post!', upvotes: 0});
+  };
 }]);
