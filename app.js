791db412d3d6ab6f6a88272fa5b26cb62a76daa0
jfmolano1587 <jose.francisco.molano@gmail.com>
2016-12-25 20:33:21 -0500

    Service added

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 var app = angular.module('flapperNews', []);
 
-app.controller('MainCtrl', [
-'$scope',
-function($scope){
-  $scope.test = 'Hello world!';
-  $scope.posts = [
+app.factory('posts', [function(){
+  var o = {
+    posts: [
   {title: 'post 1', upvotes: 5},
   {title: 'post 2', upvotes: 2},
   {title: 'post 3', upvotes: 15},
   {title: 'post 4', upvotes: 9},
   {title: 'post 5', upvotes: 4}
-  ];
+  ]
+  };
+  return o;
+}]);
+
+app.controller('MainCtrl', [
+'$scope', 'posts',
+function($scope, posts){
+  $scope.test = 'Hello world!';
+  $scope.posts = posts.posts;
   $scope.addPost = function(){
   	if(!$scope.title || $scope.title === '') { return; }
   	$scope.posts.push({title: $scope.title, upvotes: 0});
@@ -20,3 +27,4 @@ function($scope){
   post.upvotes += 1;
   };
 }]);
+
